a659e00a922aabd30d56da3af6001befdfd68c43
Bilal ALOUI <bilal.aloui.pro@gmail.com>
2025-01-11 20:32:33 +0100

    Implémenter une fonction générique de recherche par ID  Implémenter une fonction générique de sauvegarde

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -1,14 +1,23 @@
 // Question: Pourquoi créer des services séparés ?
 // Réponse: Pour mieux organiser le code, de le rendre plus modulaire et réutilisable. Cela facilite également les tests unitaires et l'entretien du code en isolant les différentes responsabilités.
 
+
 const { ObjectId } = require('mongodb');
+const db = require('../config/db');
 
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
-  // TODO: Implémenter une fonction générique de recherche par ID
+  // Implémenter une fonction générique de recherche par ID
+  return db.getDb().collection(collection).findOne({ _id: ObjectId(id) });
+}
+
+async function save(collection, data) {
+  return db.getDb().collection(collection).insertOne(data);
 }
 
 // Export des services
 module.exports = {
-  // TODO: Exporter les fonctions utilitaires
+  // Exporter les fonctions utilitaires
+  findOneById,
+  save
 };
