bb5b4881e3835f3df16992ea0adea70048af3483
Bilal ALOUI <bilal.aloui.pro@gmail.com>
2025-01-11 20:30:57 +0100

    Implémenter la création d'un cours

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -3,17 +3,46 @@
 // Question : Pourquoi séparer la logique métier des routes ?
 // Réponse : Séparer la logique métier des routes permet de rendre le code plus modulaire, maintenable et réutilisable.
 
-const { ObjectId } = require('mongodb');
-const db = require('../config/db');
+
 const mongoService = require('../services/mongoService');
 const redisService = require('../services/redisService');
+const db = require('../config/db');
+
+const collectionName = 'courses';
 
 async function createCourse(req, res) {
-  // TODO: Implémenter la création d'un cours
+  // Implémenter la création d'un cours
   // Utiliser les services pour la logique réutilisable
+
+  try {
+    const { title, description, instructor } = req.body;
+    if (!title || !description || !instructor) {
+      return res.status(400).json({ message: 'Titre, description et instructeur sont requis.' });
+    }
+
+    const newCourse = await mongoService.save(collectionName, { title, description, instructor });
+    await redisService.cacheData(collectionName + ":" + newCourse._id, newCourse , 3600);
+
+    res.status(201).json({ message: 'Cours créé avec succès.', course: newCourse });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la création du cours.', error: error.message });
+  }
+
+}
+
+async function getCourse(req, res) {
+
+}
+
+async function getCourseStats(req, res) {
+
 }
 
 // Export des contrôleurs
 module.exports = {
-  // TODO: Exporter les fonctions du contrôleur
+  // Exporter les fonctions du contrôleur
+  createCourse,
+  getCourse,
+  getCourseStats
+
 };
