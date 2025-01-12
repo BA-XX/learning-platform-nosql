eafdfcf735ec6eb2dcc09e82748febe05711d02f
Bilal ALOUI <bilal.aloui.pro@gmail.com>
2025-01-12 18:53:01 +0100

    Implémenter une fonction générique pour compter les documents Implémenter une fonction générique pour agréger les données

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -8,16 +8,29 @@ const db = require('../config/db');
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
   // Implémenter une fonction générique de recherche par ID
-  return db.getDb().collection(collection).findOne({ _id: ObjectId(id) });
+  return db.getDb().collection(collection).findOne({ _id: ObjectId.createFromHexString(id) });
 }
 
 async function save(collection, data) {
+  // Implémenter une fonction générique de sauvegarde
   return db.getDb().collection(collection).insertOne(data);
+}
+
+async function countDocuments(collection) {
+  // Implémenter une fonction générique pour compter les documents
+  return db.getDb().collection(collection).countDocuments();
+}
+
+async function aggregate(collection, pipeline) {
+  // Implémenter une fonction générique pour agréger les données
+  return db.getDb().collection(collection).aggregate(pipeline).toArray();
 }
 
 // Export des services
 module.exports = {
   // Exporter les fonctions utilitaires
   findOneById,
-  save
+  save,
+  countDocuments,
+  aggregate
 };
